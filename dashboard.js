6ddba4a73fda40c7c73089a2431400f1b3a56e1c
SAFRAN <89980850+S92064060@users.noreply.github.com>
2022-08-29 13:22:38 +0530

    Add files via upload

--- dashboard.js
+++ dashboard.js
@@ -1,21 +1,24 @@
 import React from "react";
 import {View, Text, StyleSheet, TouchableOpacity, TextInput, Image, ImageBackground} from 'react-native';
 
-const DashBoard = ({navigation}) =>{
+const DashBoard = ({navigation}) => {
     return (
         <View style={styles.container}>
-            <View style={styles.container}>
-            
-                    <TextInput style={styles.textField} placeholder={'Book Name'}/>
-                    <TextInput style={styles.textField} placeholder={'Book Author'}/>
-       
-                </View>
+              <Image style={styles.titleImage} source={{uri:"https://cdn.dribbble.com/users/6864812/screenshots/14994170/media/db2d45859cd6f9745355502112b89bb1.png"}}/>
 
-                
+              <ImageBackground source={Image} resizeMode="cover" style={styles.Image}>
+      <Text style={{color:"#8b0000", fontSize:55}}>SL GAS</Text>
+    </ImageBackground>
 
-                <TouchableOpacity style={styles.btn} onPress={()=>navigation.navigate("details")}>
-                    <Text style={styles.btnText}>Search</Text>
-                </TouchableOpacity>
+            <TouchableOpacity style={styles.btn} onPress={()=>navigation.navigate("details")}>
+                <Text style={styles.btnText}>LP GAS</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.btn} onPress={()=>navigation.navigate("SignIn")}>
+                <Text style={styles.btnText}>Live Queue Map</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.btn} onPress={()=>navigation.navigate("SignIn")}>
+                <Text style={styles.btnText}>Notifications</Text>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -23,46 +26,41 @@ const DashBoard = ({navigation}) =>{
 const styles=StyleSheet.create({
     container: {
         flex: 1,
+        alignItems: "center",
         justifyContent: "center",
-        backgroundColor: "#bda685"
+        backgroundColor: "#FF6347"
     },
-    
+    titleImage: {
+        width: 100,
+        height: 100,
+        alignSelf: "center",
+        flex: 1,
+        backgroundColor: "black",
+    },
     textScr: {
         color: "white",
         fontSize: 20,
     },
     inputContainer: {
         alignItems:"center",
-        width: '100%',
-        paddingTop: "10%",
-        paddingBottom: "30%",
-        
-    },
-    textField: {
-        padding: "10%",
-        borderRadius: 6,
-        backgroundColor:"white",
-        marginBottom: 30,
-
-       
+        width: '80%',
+        paddingTop: "2%",
+        paddingBottom: "2%",
     },
    
     btn: {
-        marginBottom: 10,
-        backgroundColor: '#009933',
+        marginTop: 20,
+        backgroundColor: '#7f678e',
         borderRadius: 10,
-        width: 370,
+        width: 250,
         alignItems: "center"
-    
     },
     btnText: {
         color: "white",
         fontSize: 20,
         fontWeight: "bold",
-        padding: 10,
-        alignItems: "center"
+        padding: 10
     }
-   
 })
 
 export default DashBoard
